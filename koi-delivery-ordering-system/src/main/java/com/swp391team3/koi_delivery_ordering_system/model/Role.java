cd7a552e7bc1719ead570531bc07c25fdb3ca6de
Khoa-Cifer <ulquiorra10203@gmail.com>
2024-09-13 21:19:26 +0700

    update code
    create model, init user and role

---
+++ koi-delivery-ordering-system/src/main/java/com/swp391team3/koi_delivery_ordering_system/model/Role.java
@@ -0,0 +1,25 @@
+package com.swp391team3.koi_delivery_ordering_system.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+
+    @OneToMany(mappedBy = "role")
+    private Set<User> users;
+}
---
+++ koi-delivery-ordering-system/src/main/java/com/swp391team3/koi_delivery_ordering_system/model/User.java
@@ -0,0 +1,37 @@
+package com.swp391team3.koi_delivery_ordering_system.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    public User(String firstName, String middleName, String lastName, String email, String password, Role role) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+}
