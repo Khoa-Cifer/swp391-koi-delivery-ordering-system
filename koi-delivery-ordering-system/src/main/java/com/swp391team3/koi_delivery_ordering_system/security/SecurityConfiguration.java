cbeb3fd3192857a8290b9bc0d59885d6ba3b4746
Khoa-Cifer <ulquiorra10203@gmail.com>
2024-09-13 21:20:37 +0700

    create configuration for security

---
+++ koi-delivery-ordering-system/src/main/java/com/swp391team3/koi_delivery_ordering_system/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.swp391team3.koi_delivery_ordering_system.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer :: disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**")
+                        .permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+}
