ac11e1a8edcf29e1eb98a9e5c629ee9b5037996d
Khoa-Cifer <ulquiorra10203@gmail.com>
2024-09-13 16:17:21 +0700

    init project

---
+++ koi-delivery-ordering-system/src/main/java/com/swp391team3/koi_delivery_ordering_system/KoiDeliveryOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.swp391team3.koi_delivery_ordering_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoiDeliveryOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoiDeliveryOrderingSystemApplication.class, args);
+	}
+
+}
