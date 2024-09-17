958241cfe02ec52036287e9a1dfefbda5fb2e9f8
Khoa-Cifer <ulquiorra10203@gmail.com>
2024-09-17 21:28:29 +0700

    update repository

--- koi-delivery-ordering-system/src/main/java/com/swp391team3/koi_delivery_ordering_system/repository/DeliveryStaffRepository.java
+++ koi-delivery-ordering-system/src/main/java/com/swp391team3/koi_delivery_ordering_system/repository/DeliveryStaffRepository.java
@@ -2,6 +2,11 @@ package com.swp391team3.koi_delivery_ordering_system.repository;
 
 import com.swp391team3.koi_delivery_ordering_system.model.DeliveryStaff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DeliveryStaffRepository extends JpaRepository<DeliveryStaff, Long> {
+    boolean existsByEmail(String email);
+
+    @Query("SELECT ds FROM DeliveryStaff ds WHERE ds.email like :email")
+    DeliveryStaff findDeliveryStaffByEmail(String email);
 }
