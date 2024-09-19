3aead0e013d7889054cea10baa041d01474e1a41
Khoa-Cifer <ulquiorra10203@gmail.com>
2024-09-19 23:18:31 +0700

    update skeleton template

---
+++ koi-delivery-ordering-system/src/main/java/com/swp391team3/koi_delivery_ordering_system/controller/DeliveringTypeController.java
@@ -0,0 +1,12 @@
+package com.swp391team3.koi_delivery_ordering_system.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/delivering-type")
+@RequiredArgsConstructor
+public class DeliveringTypeController {
+
+}
---
+++ koi-delivery-ordering-system/src/main/java/com/swp391team3/koi_delivery_ordering_system/repository/ThirdDelivererRepository.java
@@ -0,0 +1,7 @@
+package com.swp391team3.koi_delivery_ordering_system.repository;
+
+import com.swp391team3.koi_delivery_ordering_system.model.ThirdDeliverer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ThirdDelivererRepository extends JpaRepository<ThirdDeliverer, Long> {
+}
