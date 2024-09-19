6167be7f058dc2e53f67b02a674efc56a5aba068
Anh140402 <nguyenhoangmaianh855@gmail.com>
2024-09-19 13:30:29 +0700

    admin-page

--- koi-delivery-ordering-system-client/src/App.jsx
+++ koi-delivery-ordering-system-client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/login";
 import Home from "./pages/home/home";
 import Register from "./pages/register/register";
+import Admin from "./pages/Manager/Admin";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
   );
