e2d9898202ebda9a374a20e498e8c713b7d44a5b
Hungdo33 <hungdo31102003h@gmail.com>
2024-09-26 17:16:52 +0700

    update delivery staff

--- koi-delivery-ordering-system-client/src/App.jsx
+++ koi-delivery-ordering-system-client/src/App.jsx
@@ -5,11 +5,11 @@ import SignUp from "./pages/register/register";
 import Admin from "./pages/Manager/Admin";
 import Customer from "./pages/Customer/Customer";
 import EditCustomerProfile from "./pages/Customer/pages/EditCustomerProfile";
-import DeliveryStaff from "./pages/DeliveryStaff/Delivery_staff/Delivery_staff";
 import AuthProvider from "./authentication/AuthProvider";
 import PrivateRoute from "./authentication/PrivateRoute";
-
-
+import DeliveryStaff from "./pages/Manager/components/DeliveryStaff/DeliveryStaff";
+import DeliveryOrderHome from "./pages/DeliveryStaff/delivery_order_home/DeliveryOrderHome";
+import OrderAvailable from "./pages/DeliveryStaff/delivery_available_order/OrderAvailable";
 function App() {
   return (
     <AuthProvider>
@@ -20,36 +20,49 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<SignUp />} />
 
-            <Route path="/admin" element={
-              <PrivateRoute allowedRoles={4}>
-                <Admin />
-              </PrivateRoute>
-            } />
+            <Route path="/deliveryOrderHome" element={<DeliveryOrderHome />} />
+            <Route path="/orderAvailable" element={<OrderAvailable />} />
+
+            <Route
+              path="/admin"
+              element={
+                <PrivateRoute allowedRoles={4}>
+                  <Admin />
+                </PrivateRoute>
+              }
+            />
             {/* <Route path="/admin" element={<Admin />} /> */}
-            <Route path="/delivery-staff" element={
-              <PrivateRoute allowedRoles={3}>
-                <DeliveryStaff />
-              </PrivateRoute>
-            } />
+            <Route
+              path="/delivery-staff"
+              element={
+                <PrivateRoute allowedRoles={3}>
+                  <DeliveryStaff />
+                </PrivateRoute>
+              }
+            />
 
-            <Route path="/customer-edit-profile" element={
-              <PrivateRoute allowedRoles={1}>
-                <EditCustomerProfile />
-              </PrivateRoute>
-            } />
+            <Route
+              path="/customer-edit-profile"
+              element={
+                <PrivateRoute allowedRoles={1}>
+                  <EditCustomerProfile />
+                </PrivateRoute>
+              }
+            />
 
-            <Route path="/customer-home" element={
-              <PrivateRoute allowedRoles={1}>
-                <Customer />
-              </PrivateRoute>
-            } />
-
+            <Route
+              path="/customer-home"
+              element={
+                <PrivateRoute allowedRoles={1}>
+                  <Customer />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </Router>
       </main>
     </AuthProvider>
   );
 }
-
 
 export default App;
