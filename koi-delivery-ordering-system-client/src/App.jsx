ae1170e81b5ef16bf532bddbda8d2b3137a94291
Anh140402 <nguyenhoangmaianh855@gmail.com>
2024-10-03 00:17:04 +0700

    update code

--- koi-delivery-ordering-system-client/src/App.jsx
+++ koi-delivery-ordering-system-client/src/App.jsx
@@ -7,15 +7,16 @@ import EditCustomerProfile from "./pages/Customer/components/EditCustomerProfile
 import DeliveryStaff from "./pages/DeliveryStaff/home_delivery/delivery_staff/DeliveryStaff";
 import AuthProvider from "./authentication/AuthProvider";
 import PrivateRoute from "./authentication/PrivateRoute";
-import OrderAvailable from "./pages/DeliveryStaff/delivery_available_order/OrderAvailable"
-import DeliveryOrderHome from "./pages/DeliveryStaff/delivery_order_home/DeliveryOrderHome"
+import OrderAvailable from "./pages/DeliveryStaff/delivery_available_order/OrderAvailable";
+import DeliveryOrderHome from "./pages/DeliveryStaff/delivery_order_home/DeliveryOrderHome";
 import LoginCustomer from "./pages/login/LoginCustomer/LoginCustomer";
 import LoginSaleStaff from "./pages/login/LoginSaleStaff/LoginSaleStaff";
 import LoginDeliveryStaff from "./pages/login/LoginDeliveryStaff/LoginDeliveryStaff";
 import DeliveryOrderList from "./pages/DeliveryStaff/delivery_order_list/DeliveryOrderList";
 import DeliveryOrderDetail from "./pages/DeliveryStaff/delivery_order_detail/DeliveryOrderDetail";
 import DeliveryFishDetail from "./pages/DeliveryStaff/delivery_fish_detail/DeliveryFishDetail";
-import LoginAdmin from "./pages/login/LoginAdmin/LoginAdmin";
+import Used_sales_staff from "./pages/SalesStaff/UsedSalesStaff/Used_sales_staff";
+import LoginAdmin from "./pages/login/Loginadmin/LoginAdmin";
 
 function App() {
   return (
@@ -28,71 +29,102 @@ function App() {
 
             <Route path="/login-customer" element={<LoginCustomer />} />
             <Route path="/login-sales-staff" element={<LoginSaleStaff />} />
-            <Route path="/login-delivery-staff" element={<LoginDeliveryStaff />} />
+            <Route
+              path="/login-delivery-staff"
+              element={<LoginDeliveryStaff />}
+            />
             <Route path="/login-admin" element={<LoginAdmin />} />
 
-            <Route path="/admin" element={
-              <PrivateRoute allowedRoles={4}>
-                <Admin />
-              </PrivateRoute>
-            } />
+            {/* <Route
+              path="/admin"
+              element={
+                <PrivateRoute allowedRoles={4}>
+                  <Admin />
+                </PrivateRoute>
+              }
+            /> */}
 
             <Route path="/admin" element={<Admin />} />
-            <Route path="/delivery-staff" element={
-              <PrivateRoute allowedRoles={3}>
-                <DeliveryStaff />
-              </PrivateRoute>
-            } />
+            <Route
+              path="/delivery-staff"
+              element={
+                <PrivateRoute allowedRoles={3}>
+                  <DeliveryStaff />
+                </PrivateRoute>
+              }
+            />
 
-            <Route path="/customer-edit-profile" element={
-              <PrivateRoute allowedRoles={1}>
-                <EditCustomerProfile />
-              </PrivateRoute>
-            } />
+            <Route
+              path="/customer-edit-profile"
+              element={
+                <PrivateRoute allowedRoles={1}>
+                  <EditCustomerProfile />
+                </PrivateRoute>
+              }
+            />
 
+            <Route
+              path="/customer-home"
+              element={
+                <PrivateRoute allowedRoles={1}>
+                  <Customer />
+                </PrivateRoute>
+              }
+            />
 
-            <Route path="/customer-home" element={
-              <PrivateRoute allowedRoles={1}>
-                <Customer />
-              </PrivateRoute>
-            } />
+            <Route
+              path="/delivery-order-home"
+              element={
+                <PrivateRoute allowedRoles={2}>
+                  <DeliveryOrderHome />
+                </PrivateRoute>
+              }
+            />
 
-            <Route path="/delivery-order-home" element={
-              <PrivateRoute allowedRoles={2}>
-                <DeliveryOrderHome />
-              </PrivateRoute>
-            } />
+            <Route
+              path="/order-available"
+              element={
+                <PrivateRoute allowedRoles={2}>
+                  <OrderAvailable />
+                </PrivateRoute>
+              }
+            />
 
+            <Route
+              path="/delivery-order-list"
+              element={
+                <PrivateRoute allowedRoles={2}>
+                  <DeliveryOrderList />
+                </PrivateRoute>
+              }
+            />
 
-            <Route path="/order-available" element={
-              <PrivateRoute allowedRoles={2}>
-                <OrderAvailable />
-              </PrivateRoute>
-            } />
+            <Route
+              path="/delivery-order-detail"
+              element={
+                <PrivateRoute allowedRoles={2}>
+                  <DeliveryOrderDetail />
+                </PrivateRoute>
+              }
+            />
 
-            <Route path="/delivery-order-list" element={
-              <PrivateRoute allowedRoles={2}>
-                <DeliveryOrderList />
-              </PrivateRoute>
-            } />
+            <Route
+              path="/delivery-fish-detail"
+              element={
+                <PrivateRoute allowedRoles={2}>
+                  <DeliveryFishDetail />
+                </PrivateRoute>
+              }
+            />
 
-            <Route path="/delivery-order-detail" element={
-              <PrivateRoute allowedRoles={2}>
-                <DeliveryOrderDetail />
-              </PrivateRoute>
-            } />
+            */ Sale-staff
+            <Route path="/used-sales-staff" element={<Used_sales_staff />} />
 
-            <Route path="/delivery-fish-detail" element={
-              <PrivateRoute allowedRoles={2}>
-                <DeliveryFishDetail />
-              </PrivateRoute>
-            } />
           </Routes>
         </Router>
       </main>
     </AuthProvider>
   );
 }
-
 
 export default App;
---
+++ koi-delivery-ordering-system-client/src/pages/SalesStaff/UsedSalesStaff/Used_sales_staff.jsx
@@ -0,0 +1,26 @@
+import { Box } from "@mui/material";
+import Card from "../SalesComponent/Card/Card";
+import Header from "../SalesComponent/Header/Header";
+import Sidebar from "../SalesComponent/Sidebar/Sidebar";
+
+function Sales_staff_home() {
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      {/* Header */}
+      <Header />
+
+      <Box sx={{ display: "flex", flex: 1 }}>
+        {/* Main Content */}
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, p: 2, mt: "64px", display: "flex" }}
+        >
+          <Sidebar />
+          <Card />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default Sales_staff_home;
