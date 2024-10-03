891c6e056f14da6c43afd15079fbf2a5357e2da3
Anh140402 <nguyenhoangmaianh855@gmail.com>
2024-10-03 16:52:19 +0700

    sales-staff-home

---
+++ koi-delivery-ordering-system-client/src/pages/SalesStaff/UsedSalesStaff/Card/Card.jsx
@@ -0,0 +1,52 @@
+import "./Card.scss";
+
+const Card = () => {
+  return (
+    <div className="content-container" > 
+      <div className ="order-row">
+        <div className="order-card">
+          <h5 className="card-title">Order Name</h5>
+          <p className="card-text">Last Updated Date: 2024-09-26</p>
+          <p className="card-text">Destination Address: ABC Street</p>
+          <div className="button-container">
+            <button className="status-btn">Order Status</button>
+            <button className="detail-btn">Detail</button>
+          </div>
+        </div>
+
+        <div className="order-card">
+          <h5 className="card-title">Order Name</h5>
+          <p className="card-text">Last Updated Date: 2024-09-26</p>
+          <p className="card-text">Destination Address: ABC Street</p>
+          <div className="button-container">
+            <button className="status-btn">Order Status</button>
+            <button className="detail-btn">Detail</button>
+          </div>
+        </div>
+
+        <div className="order-card">
+          <h5 className="card-title">Order Name</h5>
+          <p className="card-text">Last Updated Date: 2024-09-26</p>
+          <p className="card-text">Destination Address: ABC Street</p>
+          <div className="button-container">
+            <button className="status-btn">Order Status</button>
+            <button className="detail-btn">Detail</button>
+          </div>
+        </div>
+
+        <div className="order-card">
+          <h5 className="card-title">Order Name</h5>
+          <p className="card-text">Last Updated Date: 2024-09-26</p>
+          <p className="card-text">Destination Address: ABC Street</p>
+          <div className="button-container">
+            <button className="status-btn">Order Status</button>
+            <button className="detail-btn">Detail</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Card;
