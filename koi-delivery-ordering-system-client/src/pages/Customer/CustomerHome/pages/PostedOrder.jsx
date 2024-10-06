affbb07224a75eecefb92d27fa83e5736e438db3
Khoa-Cifer <ulquiorra10203@gmail.com>
2024-10-06 22:17:03 +0700

    update post order

--- koi-delivery-ordering-system-client/src/pages/Customer/CustomerHome/pages/PostedOrder.jsx
+++ koi-delivery-ordering-system-client/src/pages/Customer/CustomerHome/pages/PostedOrder.jsx
@@ -1,9 +1,13 @@
-import { Box, styled, Typography } from "@mui/material";
+import { Box, Menu, MenuItem, Paper, styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getOrdersByStatus } from "../../../../utils/axios/order";
 import { toast } from "react-toastify";
 import ToastUtil from "../../../../components/toastContainer";
 import dateTimeConvert from "../../../../components/utils";
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import RestoreFromTrashIcon from '@mui/icons-material/RestoreFromTrash';
+import EditIcon from '@mui/icons-material/Edit';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 const commonStyles = {
     bgcolor: 'background.paper',
@@ -20,6 +24,16 @@ const OrderInfoHeader = styled(Typography)(() => ({
 
 function PostedOrder() {
     const [orders, setOrders] = useState();
+    const [expandedOrderId, setExpandedOrderId] = useState(null); // To track expanded orders
+
+    const handleClick = (orderId) => {
+        // Toggle the visibility of the order table by setting the orderId
+        setExpandedOrderId(expandedOrderId === orderId ? null : orderId);
+    };
+
+    const handleClose = () => {
+        setExpandedOrderId(null);
+    };
 
     useEffect(() => {
         const postedOrderStatus = 1;
@@ -41,49 +55,98 @@ function PostedOrder() {
         <div className="customer-home-order-list">
             <ToastUtil />
             {orders && orders.map && orders.map((order, index) => (
-                <Box sx={{ ...commonStyles, borderRadius: '16px' }} className="order-box" key={order.id}>
-                    <div className="order-main-info">
-                        <div className="order-text-info">
-                            <div>
-                                <OrderInfoHeader>Name</OrderInfoHeader>
-                                <Typography>{order.name}</Typography>
+                <>
+                    <Box sx={{ ...commonStyles, borderRadius: '16px' }} className="order-box" key={order.id}>
+                        <div className="order-main-info">
+                            <div className="order-text-info">
+                                <div>
+                                    <OrderInfoHeader>Name</OrderInfoHeader>
+                                    <Typography>{order.name}</Typography>
+                                </div>
+
+                                <div>
+                                    <OrderInfoHeader>Created Date</OrderInfoHeader>
+                                    <Typography>{dateTimeConvert(order.createdDate)}</Typography>
+                                </div>
+
+                                <div>
+                                    <OrderInfoHeader>Expected Finish Date</OrderInfoHeader>
+                                    <Typography>{dateTimeConvert(order.expectedFinishDate)}</Typography>
+                                </div>
                             </div>
 
                             <div>
-                                <OrderInfoHeader>Created Date</OrderInfoHeader>
-                                <Typography>{dateTimeConvert(order.createdDate)}</Typography>
+                                <div>
+                                    <OrderInfoHeader>Sender Address</OrderInfoHeader>
+                                    <Typography>{order.senderAddress}</Typography>
+                                </div>
+                                <div>
+                                    <OrderInfoHeader>Receiver Address</OrderInfoHeader>
+                                    <Typography>{order.destinationAddress}</Typography>
+                                </div>
                             </div>
 
-                            <div>
-                                <OrderInfoHeader>Expected Finish Date</OrderInfoHeader>
-                                <Typography>{dateTimeConvert(order.expectedFinishDate)}</Typography>
+                            <div className="order-text-info">
+                                <div>
+                                    <OrderInfoHeader>Price</OrderInfoHeader>
+                                    <Typography>{Math.floor(order.price)}</Typography>
+                                </div>
+                                <div>
+                                    <OrderInfoHeader>Tracking Id</OrderInfoHeader>
+                                    <Typography>{order.trackingId}</Typography>
+                                </div>
+
+                                <div className="icon-group">
+                                    <div className="button-icon"><EditIcon /></div>
+                                    <div className="button-icon"><RestoreFromTrashIcon /></div>
+                                    <div className="button-icon" onClick={() => handleClick(order.id)}><MenuOpenIcon /></div>
+                                </div>
                             </div>
                         </div>
 
-                        <div>
-                            <div>
-                                <OrderInfoHeader>Sender Address</OrderInfoHeader>
-                                <Typography>{order.senderAddress}</Typography>
-                            </div>
-                            <div>
-                                <OrderInfoHeader>Receiver Address</OrderInfoHeader>
-                                <Typography>{order.destinationAddress}</Typography>
-                            </div>
-                        </div>
-
-                        <div className="order-text-info">
-                            <div>
-                                <OrderInfoHeader>Price</OrderInfoHeader>
-                                <Typography>{Math.floor(order.price)}</Typography>
-                            </div>
-                            <div>
-                                <OrderInfoHeader>Tracking Id</OrderInfoHeader>
-                                <Typography>{order.trackingId}</Typography>
-                            </div>
-                        </div>
-                    </div>
-                </Box>
-
+                        {expandedOrderId === order.id && (
+                            <TableContainer component={Paper} sx={{ mt: 2 }}>
+                                <Table aria-label="order-details">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>Fish Id</TableCell>
+                                            <TableCell>Name</TableCell>
+                                            <TableCell>Price</TableCell>
+                                            <TableCell>Size</TableCell>
+                                            <TableCell>Status</TableCell>
+                                            <TableCell>Weight</TableCell>
+                                            <TableCell>Action</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {order.fishes && order.fishes.map && order.fishes.map((fish, index) => (
+                                            <TableRow key={fish.id}>
+                                                <TableCell>{fish.id}</TableCell>
+                                                <TableCell>{fish.name}</TableCell>
+                                                <TableCell>{fish.price}</TableCell>
+                                                <TableCell>{fish.size}</TableCell>
+                                                {fish.status === 0 && (
+                                                    <TableCell>Unknown</TableCell>
+                                                )}
+                                                {fish.status === 1 && (
+                                                    <TableCell>Good</TableCell>
+                                                )}
+                                                {fish.status === 2 && (
+                                                    <TableCell>Sick</TableCell>
+                                                )}
+                                                {fish.status === 3 && (
+                                                    <TableCell>Dead</TableCell>
+                                                )}
+                                                <TableCell>{fish.weight}</TableCell>
+                                                <TableCell><div className="button-icon"><MoreHorizIcon /></div></TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        )}
+                    </Box>
+                </>
             ))}
         </div>
     )
