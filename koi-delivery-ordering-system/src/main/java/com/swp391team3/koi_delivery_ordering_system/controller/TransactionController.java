0f2b3d29245e70afbe2b7f4cf083b6ed479d7f1e
Khoa-Cifer <ulquiorra10203@gmail.com>
2024-10-13 20:31:54 +0700

    update transaction api

--- koi-delivery-ordering-system/src/main/java/com/swp391team3/koi_delivery_ordering_system/controller/TransactionController.java
+++ koi-delivery-ordering-system/src/main/java/com/swp391team3/koi_delivery_ordering_system/controller/TransactionController.java
@@ -1,11 +1,13 @@
 package com.swp391team3.koi_delivery_ordering_system.controller;
 
+import com.swp391team3.koi_delivery_ordering_system.model.Transaction;
 import com.swp391team3.koi_delivery_ordering_system.service.ITransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/transaction")
@@ -21,4 +23,14 @@ public class TransactionController {
     // ) {
     //     return ResponseEntity.ok(transactionService.createTransaction(id, createdDate, amount));
     // }
+
+    @GetMapping("/get-transactions-by-customer-id/{customerId}")
+    public ResponseEntity<?> getTransactionsByCustomer(@PathVariable Long customerId) {
+        return ResponseEntity.ok(transactionService.getTransactionsByCustomerId(customerId));
+    }
+
+    @GetMapping("/get-all-transactions")
+    public ResponseEntity<?> getAllTransactions() {
+        return ResponseEntity.ok(transactionService.getAllTransactionsHistory());
+    }
 }
