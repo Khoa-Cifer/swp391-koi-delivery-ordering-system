69cdd6b62a4d0deb7e2234ea1e62e72b994717b7
Anh140402 <nguyenhoangmaianh855@gmail.com>
2024-10-13 20:53:33 +0700

    Update UI

--- koi-delivery-ordering-system-client/src/utils/axios/transaction.js
+++ koi-delivery-ordering-system-client/src/utils/axios/transaction.js
@@ -2,7 +2,7 @@ import axiosClient from "../axios";
 
 export async function getAllTransaction() {
     try {
-        const response = await axiosClient.get("fishes/get-all-payment-history");
+        const response = await axiosClient.get("transaction/get-all-transactions");
         return response.data;
     } catch (error) {
         console.log(error);
