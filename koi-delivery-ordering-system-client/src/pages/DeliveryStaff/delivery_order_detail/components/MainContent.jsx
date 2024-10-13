8a918112b0334dd02a53f210af45a19d24df0ea5
Anh140402 <nguyenhoangmaianh855@gmail.com>
2024-10-13 13:34:25 +0700

    change

--- koi-delivery-ordering-system-client/src/pages/DeliveryStaff/delivery_order_detail/components/MainContent.jsx
+++ koi-delivery-ordering-system-client/src/pages/DeliveryStaff/delivery_order_detail/components/MainContent.jsx
@@ -1,9 +1,3 @@
-// import "./MainContent.scss";
-// const MainContent = () => {
-//   return (
-//     <div className="order-details-container">
-//       {/* Order Details Table */}
-
 import { Box, Button, Grid, Paper, styled } from "@mui/material";
 import { jwtDecode } from "jwt-decode";
 import { useCallback, useEffect, useState } from "react";
@@ -20,55 +14,6 @@ import CurrentPosition from "../../../../assets/delivery-current.svg"
 import { updateDeliveryStaffCurrentLocation } from "../../../../utils/axios/deliveryStaff";
 import { finishOrder } from "../../../../utils/axios/order";
 import Spinner from "../../../../components/SpinnerLoading";
-
-//       <div className="order-name-detail">
-//         <strong>Order name</strong>
-//       </div>
-//       <div className="order-status-detail">Order status</div>
-
-//       <div className="order-details">
-//         <div className="details-row">
-//           <div className="details-column">
-//             <p>Tracking Id</p>
-//             <p>Receiver Address</p>
-//             <p>Sender Address</p>
-//           </div>
-//           <div className="details-column">
-//             <p>Name</p>
-//             <p>Price</p>
-//           </div>
-//           <div className="details-column">
-//             <p>Created Date</p>
-//             <p>Last Updated Date / Finish Date</p>
-//           </div>
-//           <div className="view-fish">
-//             <button className="view-fish-btn">View Fish </button>
-//           </div>
-//         </div>
-
-//         <div className="map-placeholder">Google Map</div>
-
-//         <div className="order-description">
-//           <h4>Description</h4>
-//           <p>
-//             Con biết bây giờ mẹ chờ tin con Khi thấy mai đào nở vàng bên nương
-//             Năm trước con hẹn đầu xuân sẽ về Nay én bay đầy trước ngõ mà tin con
-//             vẫn xa ngàn xa Ôi nhớ xuân nào thuở trời yên vui Nghe pháo giao thừa
-//             rộn ràng nơi nơi Bên mái tranh nghèo ngồi quanh bếp hồng Trông bánh
-//             chưng chờ trời sáng Đỏ hây hây những đôi má đào Nếu con không về
-//             chắc mẹ buồn lắm, Mái tranh nghèo không người sửa sang Khu vườn
-//             thiếu hoa vàng mừng xuân Đàn trẻ thơ ngây chờ mong anh trai Sẽ đem
-//             về cho tà áo mới Ba ngày xuân đi khoe xóm giềng
-//           </p>
-//         </div>
-//       </div>
-
-//       {/* Google Map Section */}
-//     </div>
-//   );
-// };
-
-// export default MainContent;
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: '#fff',
