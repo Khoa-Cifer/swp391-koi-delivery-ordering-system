dc67f241d84c5803bc50cbc696709382b95d97d7
Anh140402 <nguyenhoangmaianh855@gmail.com>
2024-11-05 13:17:59 +0700

    update UI for delivery staff

--- koi-delivery-ordering-system-client/src/pages/Layout/DeliveryStaffLayout/components/Header/Header.jsx
+++ koi-delivery-ordering-system-client/src/pages/Layout/DeliveryStaffLayout/components/Header/Header.jsx
@@ -71,8 +71,8 @@ function Header() {
             </div>
 
             <div className="function">
-                <Button style={{ fontSize: "1em", height: "45px", backgroundColor: "white", color: "rgb(17, 17, 162)" }} onClick={() => navigate("/")}><strong>Home</strong></Button>
-                <Button style={{ fontSize: "1em", height: "45px", backgroundColor: "white", color: "rgb(17, 17, 162)" }} onClick={() => navigate("/support-page")}><strong>Support</strong></Button>
+                <Button style={{ fontSize: "1.1em", height: "45px", backgroundColor: "white", color: "rgb(17, 17, 162)" }} onClick={() => navigate("/")}><strong>Home</strong></Button>
+                <Button style={{ fontSize: "1.1em", height: "45px", backgroundColor: "white", color: "rgb(17, 17, 162)" }} onClick={() => navigate("/support-page")}><strong>Support</strong></Button>
             </div>
 
             <div className="logo">
