ec6c78dc5679475d4751bb0e7c95e736848a39c1
Hungdo33 <hungdo31102003h@gmail.com>
2024-11-15 10:03:15 +0700

    test deploy

--- koi-delivery-ordering-system-client/vite.config.js
+++ koi-delivery-ordering-system-client/vite.config.js
@@ -19,5 +19,6 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 5173, // you can replace this port with any port
-  }
+  },
+  base:"/vite-deploy/"
 });
